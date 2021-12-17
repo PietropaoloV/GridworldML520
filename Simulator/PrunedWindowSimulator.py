f057642636068782398e19f9a956b00afb80eaeb
Sumeet Bhatnagar <sumeetwork0@gmail.com>
2021-12-17 16:52:22 -0500

    Add Implementation for Project 2 Simulator

--- Simulator/PrunedWindowSimulator.py
+++ Simulator/PrunedWindowSimulator.py
@@ -5,6 +5,8 @@ from typing import List
 import numpy as np
 from tensorflow.keras.models import load_model
 
+from SimulatorTypes import SimulatorTypes
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import os
 
@@ -25,20 +27,25 @@ class Simulator():
     knownOutOfBounds: int = 0
     knownBumps: int = 0
     pathToSaveStats: str = None
+    simulatorType:SimulatorTypes = None 
+    cX:np.ndarray = None
 
     def __init__(
         self,
         pathToGrids,
         pathToModel,
+        simulatorType = SimulatorTypes.PROJECT1
     ) -> None:
         self.grids = np.load(pathToGrids)
         self.model = load_model(pathToModel)
         self.rowSize = len(self.grids[0])
         self.colSize = len(self.grids[0][0])
+        self.simulatorType = simulatorType 
         self.stats['trajectoryLengths'] = np.array([])
         self.stats['knownBumps'] = np.array([])
         self.stats['knownOutOfBounds'] = np.array([])
         self.stats['discardedGrids'] = np.array([])
+        self.stats['unknownBumps'] = np.array([])
 
     def generateWindow(self, windowSize):
         window = []
@@ -54,7 +61,35 @@ class Simulator():
                     windowRow.append(self.kb[rowCoord][colCoord])
             window.append(windowRow)
 
-        return np.array(window)
+        return np.reshape(np.array(window),(1,11,11))
+    
+    def generateWindowskBPlusCx(self,windowSize):
+        kbWindow = []
+        cXWindow = []
+
+        for rowCoord in range(self.currentPosition[0] - windowSize // 2,
+                              self.currentPosition[0] + windowSize // 2 + 1):
+            kbWindowRow = []
+            cXWindowRow = []
+            for colCoord in range(
+                    self.currentPosition[1] - windowSize // 2,
+                    self.currentPosition[1] + windowSize // 2 + 1):
+                if rowCoord < 0 or colCoord < 0 or rowCoord >= self.rowSize or colCoord >= self.colSize:
+                    kbWindowRow.append(-1)
+                    cXWindowRow.append(0)
+                else:
+                    kbWindowRow.append(self.kb[rowCoord][colCoord])
+                    cXWindowRow.append(self.cX[rowCoord][colCoord])
+            kbWindow.append(kbWindowRow)
+            cXWindow.append(cXWindowRow)
+
+        kbWindow = np.array(kbWindow)
+        cXWindow = np.array(cXWindow)
+        kbWindow = kbWindow.reshape((1,windowSize,windowSize)) 
+        cXWindow = cXWindow.reshape((1,windowSize,windowSize)) 
+        window = np.transpose(np.concatenate((kbWindow,cXWindow),axis = 0),[1,2,0])
+        return np.reshape(window,(-1,11,11,2))
+        # return np.reshape(np.concatenate((kbWindow,cXWindow),axis=0),(1,11,11,2))
 
     def getModel(self, pathToModelFile):
         return load_model(pathToModelFile)
@@ -79,7 +114,8 @@ class Simulator():
         return neighbors
 
     def predict(self, window):
-        predictions = self.model.predict(np.reshape(window, (1, 11, 11)))[0]
+        # print(window.shape)
+        predictions = self.model.predict(window)[0]
         # print(predictions)
         nextPosition = None
         iterationCount = 0
@@ -127,6 +163,38 @@ class Simulator():
         else:
             return nextPosition
 
+    def updateKB(self):
+        neighbors = self.getFourNeighbors(self.currentPosition[0],
+                                            self.currentPosition[1])
+
+        for neighbor in neighbors:
+
+            if not self.inBounds(neighbor[0], neighbor[1]):
+                continue
+
+            if self.grid[neighbor[0]][neighbor[1]] == 0:
+                self.kb[neighbor[0]][neighbor[1]] = 0
+            elif self.grid[neighbor[0]][neighbor[1]] == 1:
+                self.kb[neighbor[0]][neighbor[1]] = -1
+    
+    def updateCx(self):
+        neighbors = self.getEightNeighbors(self.currentPosition[0],
+                                            self.currentPosition[1])
+
+        blockedCount = 0
+
+        for neighbor in neighbors:
+
+            if not self.inBounds(neighbor[0], neighbor[1]):
+                continue
+
+            if self.grid[neighbor[0]][neighbor[1]] == 1:
+                blockedCount += 1
+            
+        
+        self.cX[self.currentPosition[0]][self.currentPosition[1]] = blockedCount
+
+
     def NRuns(self, n, saveStats=False, filePath=''):
 
         for i in range(n):
@@ -151,6 +219,9 @@ class Simulator():
 
         self.kb = np.array([[0] * self.colSize for _ in range(self.rowSize)])
 
+        if self.simulatorType == SimulatorTypes.PROJECT2:
+            self.cX = np.array([[0] * self.colSize for _ in range(self.rowSize)])
+
         self.kb[start[0]][start[1]] = 2
         self.kb[goal[0]][goal[1]] = 3
 
@@ -172,19 +243,22 @@ class Simulator():
                 self.visitedFromPoint[self.currentPosition] = []
 
             self.kb[self.currentPosition[0]][self.currentPosition[1]] = 2
-            neighbors = self.getFourNeighbors(self.currentPosition[0],
-                                              self.currentPosition[1])
-            for neighbor in neighbors:
 
-                if not self.inBounds(neighbor[0], neighbor[1]):
+            if self.simulatorType == SimulatorTypes.PROJECT1:
+                self.updateKB()
+            elif self.simulatorType == SimulatorTypes.PROJECT2:
+                if self.grid[self.currentPosition[0]][self.currentPosition[1]] == 1:
+                    self.kb[self.currentPosition[0]][self.currentPosition[1]] = -1
+                    self.currentPosition = self.parent[self.currentPosition]
                     continue
+                else:
+                    self.kb[self.currentPosition[0]][self.currentPosition[1]] = 0
+                    self.updateCx()
 
-                if self.grid[neighbor[0]][neighbor[1]] == 0:
-                    self.kb[neighbor[0]][neighbor[1]] = 0
-                elif self.grid[neighbor[0]][neighbor[1]] == 1:
-                    self.kb[neighbor[0]][neighbor[1]] = -1
-
-            window = self.generateWindow(11)
+            if self.simulatorType == SimulatorTypes.PROJECT1:
+                window = self.generateWindow(11)
+            elif self.simulatorType == SimulatorTypes.PROJECT2:
+                window = self.generateWindowskBPlusCx(11)
 
             nextPosition = self.predict(window)
 
@@ -201,7 +275,7 @@ class Simulator():
             self.kb[self.currentPosition[0]][self.currentPosition[1]] = 0
             self.kb[nextPosition[0]][nextPosition[1]] = 2
             self.currentPosition = nextPosition
-            # print(self.currentPosition)
+            print(self.currentPosition)
 
         if discardGrid:
             self.stats['discardedGrids'] = np.append(
@@ -218,16 +292,16 @@ class Simulator():
 if __name__ == '__main__':
 
     pathToGridFile = os.path.join(os.path.dirname(__file__),
-                                  '../grids/project1-grids.npy')
+                                  '../grids/project2-grids.npy')
 
     # print(grids.shape)
 
-    modelFilePath = os.path.join(os.path.dirname(__file__), '../CNNModel')
+    modelFilePath = os.path.join(os.path.dirname(__file__), '../p2-cnn-2')
 
-    simulator = Simulator(pathToGridFile, modelFilePath)
+    simulator = Simulator(pathToGridFile, modelFilePath,simulatorType=SimulatorTypes.PROJECT2)
     # simulator.OneRunOnRandomGrid()
-    simulator.NRuns(1000, True,
-                    os.path.join(os.path.dirname(__file__), 'statsDump'))
+    simulator.NRuns(1, True,
+                    os.path.join(os.path.dirname(__file__), 'statsDumpProject2CNN'),)
     # simulator.saveStats(os.path.dirname(__file__))
     # print(simulator.trajectoryLengths)
     # print(simulator.deadNodes)
---
+++ Simulator/SimulatorTypes.py
@@ -0,0 +1,5 @@
+from enum import Enum
+
+class SimulatorTypes(Enum):
+    PROJECT2 = 2
+    PROJECT1 = 1
